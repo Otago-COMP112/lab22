0df38b52b237889469dd07222af7b67a69d68f3b
Tin Htoo Aung <tinhtooaung@oucs1524.otago.ac.nz>
2015-05-15 12:18:25 +1200

    submission version

--- script.js
+++ script.js
@@ -10,32 +10,37 @@ if(document.getElementById){
 }
 
 function validateForm() {
-    var isValid = false;
+    var isValid = true;
     var errors = [];
     removeErrors();
     var name  = document.forms["contactForm"]["name"].value;
     if (name == null || name == "") {
+        isValid = false
         errors.push("Name is required");
         document.getElementById("labelName").classList.add('input-error')
     }
 
     var email  = document.forms["contactForm"]["email"].value;
     if (email == null || email == "") {
+        isValid = false
         errors.push("Email is required");
         document.getElementById("labelEmail").classList.add('input-error')
     }else if(!validateEmail(email)){
+        isValid = false
         errors.push("Invalid email address");
         document.getElementById("labelEmail").classList.add('input-error')
     }
 
     var subject  = document.forms["contactForm"]["subject"].value;
     if (subject == null || subject == "") {
+        isValid = false
         errors.push("Subject is required");
         document.getElementById("labelSubject").classList.add('input-error')
     }
 
     var message  = document.forms["contactForm"]["message"].value;
     if (message == null || message == "") {
+        isValid = false
         errors.push("Message is required");
         document.getElementById("labelMessage").classList.add('input-error')
     }
